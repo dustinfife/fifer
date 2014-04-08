fbd1ce51234af8fa9729ae47d7c905a55f2c34d5
Dustin Fife <fife.dustin@gmail.com>
2014-04-08 12:01:50 -0500

    Created read.fife and write.fife
    read.fife and write.fife add a meta file that saves the original file
    name/location of the raw data matrix.

--- R/meta.data.R
+++ R/meta.data.R
@@ -1,26 +1,53 @@
-##' Load the meta-data for a file
+##' Read in a dataset and load the meta-data for a file
 ##'
-##' \code{read.fife}
+##' Oftentimes the original data matrix is too large to practically read in everytime you want to do analysis.
+##' This often means creating a separate file for analysis. Unfortunately, if the original file is changed, the separate
+##' file doesn't reflect those changes. \code{read.fife} and \code{\link{write.fife}} both read and write meta-data,
+##' then display the original file name for the meta data.
 ##'	
-##' Write the details here
-##' @param
-##' @param
-##' @aliases
-##' @seealso \code{\link{MASS}}
-##' @references
-##' @return \item{}{}
-##' @author
+##' Technically, \code{read.fife} and \code{\link{write.fife}} don't actually read and write meta-data. Instead, they create
+##' (or read) a separate file that has the same name (though different extension) than the subsetted dataset. The extension of 
+##' the meta data file is .file. 
+##' @param file The location of the file to be read.
+##' @param file.type The extension of the dataset. Defaults to .csv.
+##' @param ... other arguments passed to to \code{\link{read.csv}}
+##' @aliases read.meta read.meta.data read.metadata read.fifer
+##' @seealso \code{\link{write.fife}}
+##' @return An R object containing the subsetted dataset. Also, an object called \code{original.file} will be loaded into the R 
+##' environment that contains a string of the original file location
+##' @author Dustin Fife
 ##' @export
-##' @examples
 read.fife = function(file, file.type = ".csv", ...){
 	data = read.csv(file, ...)
 	#### extract the meta data
 	meta = gsub(file.type, ".file", file, fixed=T)
-	loaded.data = load(meta, verbose=T)
+	load(meta, verbose=T,envir=.GlobalEnv)
 	cat(original.file)
+	return(data)
 }
 
 
+##' Write a dataset and load the meta-data
+##'
+##' Oftentimes the original data matrix is too large to practically read in everytime you want to do analysis.
+##' This often means creating a separate file for analysis. Unfortunately, if the original file is changed, the separate
+##' file doesn't reflect those changes. \code{read.fife} and \code{\link{write.fife}} both read and write meta-data,
+##' then display the original file name for the meta data.
+##'	
+##' Technically, \code{read.fife} and \code{\link{write.fife}} don't actually read and write meta-data. Instead, they create
+##' (or read) a separate file that has the same name (though different extension) than the subsetted dataset. The extension of 
+##' the meta data file is .file. 
+##' @param object An R object to be written as a .csv (or whatever) file.
+##' @param newfile The location of the subsetted dataset to be written.
+##' @param originalfile The location of the original file that was subsetted. 
+##' @param file.type The file type to be read. Defaults to .csv.
+##' @param row.names Should row names be written? Defaults to FALSE.
+##' @param fullpath Should the full path be written to the meta-data? (e.g., "documents/research/datasets/medical_data_ap9_2014.csv"). Defaults to T.
+##' @param ... Other arguments passed to \code{\link{write.csv}}.
+##' @aliases write.meta write.meta.data write.metadata write.fifer
+##' @seealso \code{\link{read.fife}}
+##' @author Dustin Fife
+##' @export
 write.fife = function(object, newfile, originalfile=NULL, file.type = ".csv", row.names=F, fullpath=T,...){
 	
 	#### bark if they don't supply originalfile name
