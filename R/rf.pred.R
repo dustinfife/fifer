1c644ef9117d4225c8ead6acb5bf76df954688c4
Dustin Fife <fife.dustin@gmail.com>
2014-05-15 08:58:55 -0500

    Put the random forest stuff back in

---
+++ R/rf.pred.R
@@ -0,0 +1,294 @@
+##' Variable selection for prediction purposes using Random Forest. See \code{\link{rfThresh}} for complete documentation.
+##'
+##' @title Variable selection in Random Forest
+##' @param object an object returned from \code{\link{rfInterp}}
+##' @param importance what importance measure should be used? Either "permutation" or "gini."
+##' @param nfor.pred number of forests to grow
+##' @param nmj a contant used for setting the threshold for variable selection. Higher values indicate a less stringent threshold. 
+##' @param ... other arguments passed to \code{\link{cforest}} or \code{\link{randomForest}}
+#'@return \item{varselect.pred}{The variables selected for Prediction (sorted)}
+#'@return \item{err.interp}{The error at each stage of the stepwise variable inclusion.}
+#'@return \item{mean.jump}{The threshold for variable inclusion.}
+#'@return \item{stepwise.error}{The OOB error rate at each iteration of the stepwise procedure.}
+#'@return \item{num.varselect.pred}{The number of variables selected for prediction.}
+#'@return \item{comput.time}{Computation time of the procedure.}
+#'@return \item{model}{The final model, either a \code{randomForest} or \code{cforest} object.}
+#' @author Robin Genuer, Jean-Michel Poggi and Christine Tuleau-Malot, with modifications by Dustin Fife
+#' @seealso \code{\link{rfInterp}}, \code{\link{rfThresh}}
+#' @rdname rfPred
+#' @export rfPred
+##' @examples
+#' \dontrun{data(iris); 
+#' data = iris; 
+#' formula = as.formula("Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width")
+#' thresh = rfThresh(formula, data=iris, nruns=2, importance="permutation"); 
+#' interp = rfInterp(thresh, importance="permutation");
+#' predic = rfPred(interp, importance="gini")
+#' predic}
+rfPred <-function(object, importance="permutation", nfor.pred=25, nmj=1, ...){
+
+
+	### record system time at beginning
+	start = Sys.time()
+	formula = object$formula
+			
+	##### extract variable names and OOB values	
+	err.interp = object$err.interp
+	varselect.interp = object$varselect.interp
+	data = object$data
+				
+	##### extract IV/DV
+	x.lab = attr(terms(formula), "term.labels")
+	x = data[,x.lab]
+	y.lab = row.names(attr(terms(formula), "factors"))[1]
+	y = data[,y.lab]
+	
+	##### extract other things
+	vars = attr(terms(formula), "term.labels")
+	
+
+	#### get sample size and number of variables
+	nvars = length(vars)
+	n = nrow(x)
+
+
+
+	k <- length(err.interp)
+	l <- length(varselect.interp)
+
+	if (l==1) {
+		warning(
+		"Unable to perform prediction step, because the interpretation step
+		did not eliminate variables")
+		varselect.pred <- NULL
+		err.pred <- NULL
+		mean.jump <- NULL
+	} else {
+
+		# mean jump calculation
+		s=NULL
+		for (i in l:(k-1)){
+			s <- c(s, abs(err.interp[i+1] - err.interp[i]) )
+		}
+		
+		mean.jump <- mean(s, na.omit=T)
+		
+		# comparison between the error with the variable and the precedent error
+		# and test of the addition of the variable
+		rf <- rep(NA, nfor.pred)
+
+		##### fit it for only first variable
+		if (importance=="permutation"){		
+			for (j in 1:nfor.pred) {
+				y = row.names(attr(terms(formula), "factors"))[1]
+				f = make.formula(y, vars[1])
+				rfmod = cforest(f, data=data, controls=cforest_control(mtry=1), ...)
+				oob = predict(rfmod, OOB=T); oob = 1-length(which(oob==data[,y]))/length(data[,y], ...)
+				rf[j] <- oob
+			}
+		} else {
+			for (j in 1:nfor.pred) {
+				y = row.names(attr(terms(formula), "factors"))[1]
+				f = make.formula(y, vars[1])
+				if (object$model$type=="regression"){
+					rf[j] <- tail(randomForest(f, data=data, ...)$mse, n=1)
+				} else {
+					rf[j] <- tail(randomForest(f, data=data, ...)$err.rate[,1], n=1)					
+				}
+			}
+		}
+		err.pred <- mean(rf)
+
+
+		t <- err.pred
+		
+		stepwise.error = 1:l
+		stepwise.error[1] = t
+
+		### if there's more than one variable
+		if (l>1) { 
+			
+			#### sequentially add variables
+			varselect.pred = vars[1]
+			
+			for (i in 2:l){	
+				form.1 = make.formula(y.lab, c(varselect.pred, vars[i]))
+				#### preallocate
+				rf <- rep(NA, nfor.pred)
+				
+				#### fit just this set of variables nfor.pred times
+				for (j in 1:nfor.pred) {
+					if (importance=="permutation"){
+						rfor = cforest(form.1, data=data, controls=cforest_control(mtry=sqrt(i)), ...)
+						oob = predict(rfor, OOB=T, ...); oob = 1-length(which(oob==data[,y.lab]))/length(data[,y])
+						rf[j] = oob
+					} else {
+						if (object$model$type=="regression"){
+							rf[j] <- tail(randomForest(f, data=data, ...)$mse, n=1)
+						} else {
+							rf[j] <- tail(randomForest(f, data=data, ...)$err.rate[,1], n=1)					
+						}
+					}	
+				}
+				z <- mean(rf)
+				
+				### record error at this iteration
+				stepwise.error[i] = z
+			
+				if ((t-z) > nmj*mean.jump){
+					varselect.pred <- c(varselect.pred, vars[i])
+					err.pred <- c(err.pred, z)
+					t <- z
+				}				
+
+			}
+		}
+	}
+	formula = make.formula(y.lab, varselect.pred)
+	if (importance=="gini"){
+		model = randomForest(formula, data=data, importance=TRUE,...)
+	} else {
+		model = cforest(formula, data=data, controls=cforest_unbiased(ntree=1000, mtry=sqrt(length(varselect.pred))),...)
+	}
+		
+	comput.time <- Sys.time()-start
+	
+	output <- list(
+	'varselect.pred'=varselect.pred,
+	'vars.considered' = vars,
+	 'err.pred'=err.pred,
+	 'stepwise.error' = stepwise.error,
+	 'mean.jump'=mean.jump,
+	 'num.varselect.pred'=length(varselect.pred),
+	 'comput.time'=comput.time,
+	 'model' = model)
+	attr(output, "class") = "rfPred"
+	return(output)
+}
+
+
+
+#' @title Print rfPred Summary
+#' @aliases print.rfPred
+#' @param x an rfPred object
+#' @param ... ignored
+#' @method print rfPred
+#' @S3method print rfPred
+print.rfPred = function(x,...){
+	print(names(x))
+	cat(paste("\n\nThe remaining variables (in order of importance) are:\n\n", sep=""))
+	print(sort(x$varselect.pred, decreasing=TRUE))
+	cat(paste("\n\n"))	
+	print(x$comput.time)
+}	
+
+
+#' @title Prepare xtable Summary
+#' @aliases xtable.rfPred
+#' @param x an rfPred object
+#' @param caption Character vector of length 1 or 2 containing the table's caption or title. If length 2, the second item 
+#' is the "short caption" used when LaTeX generates a "List of Tables". Set to NULL to suppress the caption. Default value is NULL.	
+#' @param label Character vector of length 1 containing the LaTeX label
+#'    or HTML anchor. Set to \code{NULL} to suppress the label.  Default
+#'    value is \code{NULL}. 
+#' @param align Character vector of length equal to the number of columns
+#'     of the resulting table indicating the alignment of the corresponding
+#'     columns.  Also, \code{"|"} may be used to produce vertical lines
+#'     between columns in LaTeX tables, but these are effectively ignored
+#'     when considering the required length of the supplied vector.  If a
+#'     character vector of length one is supplied, it is split as
+#'     \code{strsplit(align, "")[[1]]} before processing. Since the row
+#'     names are printed in the first column, the length of \code{align} is
+#'     one greater than \code{ncol(x)} if \code{x} is a
+#'     \code{data.frame}. Use \code{"l"}, \code{"r"}, and \code{"c"} to
+#'     denote left, right, and center alignment, respectively.  Use
+#'     \code{"p\{3cm\}"} etc for a LaTeX column of the specified width. For
+#'     HTML output the \code{"p"} alignment is interpreted as \code{"l"},
+#'     ignoring the width request. Default depends on the class of
+#'     \code{x}. 
+#' @param digits Numeric vector of length equal to one (in which case it will be
+#'    replicated as necessary) or to the number of columns of the
+#'    resulting table \bold{or} matrix of the same size as the resulting
+#'    table indicating the number of digits to display in the
+#'    corresponding columns. Since the row names are printed in the first
+#'    column, the length of the vector \code{digits} or the number of
+#'    columns of the matrix \code{digits} is one greater than
+#'    \code{ncol(x)} if \code{x} is a \code{data.frame}. Default depends
+#'    of class of \code{x}. If values of \code{digits} are negative, the
+#'    corresponding values of \code{x} are displayed in scientific format
+#'    with \code{abs(digits)} digits.
+#' @param display Character vector of length equal to the number of columns of the
+#'    resulting table indicating the format for the corresponding columns.
+#'    Since the row names are printed in the first column, the length of
+#'    \code{display} is one greater than \code{ncol(x)} if \code{x} is a
+#'    \code{data.frame}.  These values are passed to the \code{formatC}
+#'    function.  Use \code{"d"} (for integers), \code{"f"}, \code{"e"},
+#'    \code{"E"}, \code{"g"}, \code{"G"}, \code{"fg"} (for reals), or
+#'    \code{"s"} (for strings).  \code{"f"} gives numbers in the usual
+#'    \code{xxx.xxx} format; \code{"e"} and \code{"E"} give
+#'    \code{n.ddde+nn} or \code{n.dddE+nn} (scientific format); \code{"g"}
+#'    and \code{"G"} put \code{x[i]} into scientific format only if it
+#'    saves space to do so.  \code{"fg"} uses fixed format as \code{"f"},
+#'    but \code{digits} as number of \emph{significant} digits.  Note that
+#'    this can lead to quite long result strings.  Default depends on the
+#'    class of \code{x}.
+#' @param ... other arguments passed to xtable
+#' @method xtable rfPred
+#' @S3method xtable rfPred
+#' @import xtable
+xtable.rfPred = function(x,caption=NULL, label=NULL, align=NULL, digits=NULL, display=NULL,...){
+	require(xtable)
+	tab = data.frame(matrix(nrow=length(x$vars.considered), ncol=3))
+	names(tab) = c("Current Variable", "OOB Error", "Model Selected")
+	tab[,1] = x$vars.considered
+	tab[,2] = x$stepwise.error
+	selected = x$varselect.pred
+	
+	#### if only one is selected, populate entire table
+	if (length(selected)==1){
+		tab[,3] = selected
+	} else {
+
+		#### give first row the first variable
+		tab[1,3] = selected[1] 
+		sel.string = selected[1]
+		
+		s=2
+		#### loop through and add only if it's included
+		for (i in 2:nrow(tab)){
+			if (s>length(selected)){
+				tab[(i:nrow(tab)),3] = sel.string
+			} else {
+				if (tab[i,1] == selected[s]){
+					sel.string = paste(sel.string, "+", selected[s])
+					s = s+1
+				}
+				tab[i,3] = sel.string
+			}
+
+		}
+	}
+	
+	xtable(tab, caption=caption, label=label, align=align, digits=digits, display=display, ...)
+
+}
+	
+
+	
+	
+#' @title Plot rfPred Summary
+#' @aliases plot.rfPred
+#' @param x an rfPred object
+#' @param y igorned
+#' @param ... other parameters passed to plo
+#' @method plot rfPred
+#' @S3method plot rfPred
+plot.rfPred = function(x, y, ...){
+	length.vars = length(x$varselect.pred)
+
+	labels = list(ylab="Stepwise OOB Error", xlab="", pch=16, cex=.8,col="gray", type="b", xaxt="n") 
+	args = modifyList(labels, list(x=1:length.vars, y=x$err.pred[1:length.vars],...))
+	do.call("plot", args)
+#	plot(1:length.vars, x$err.pred[1:length.vars], ylab="Stepwise OOB Error", xlab="", pch=16, cex=.8,col="gray", type="b",...)
+	text(1:length.vars, x$err.pred[1:length.vars], x$varselect.pred, pos=4, cex=.8)
+}
---
+++ R/rf.thresh.R
@@ -0,0 +1,229 @@
+##' Using a set of predictors, this function uses random forests to select the best ones in a stepwise fashion. Both the procedure and
+##' the algorithm were borrowed heavily from the \code{VSURF} package with some modifications. These modifications allow for unbiased
+##' computation of variable importance via the \code{\link{cforest}} function in the party package. 
+##'
+##' @details What follows is the documentation for the original algorithm in VSURF: 
+##'
+#' Three steps variable selection procedure based on random forests for
+#' supervised classification and regression problems.  First step
+#' ("thresholding step") is dedicated to eliminate irrelevant variables from
+#' the dataset.  Second step ("interpretation step") aims to select all
+#' variables related to the response for interpretation prupose.  Third step
+#' ("prediction step") refines the selection by eliminating redundancy in the
+#' set of variables selected by the second step, for prediction prupose.
+#' 
+#' \itemize{ \item First step ("thresholding step"): first, \code{nfor.thres}
+#' random forests are computed using the function \code{randomForest} with
+#' arguments \code{importance=TRUE}. Then variables are sorted according to
+#' their mean variable importance (VI), in decreasing order.  This order is
+#' kept all along the procedure. Next, a threshold is computed:
+#' \code{min.thres}, the minimum predicted value of a pruned CART tree fitted
+#' to the curve of the standard deviations of VI.  Finally, the actual
+#' "thresholding step" is performed: only variables with a mean VI larger than
+#' \code{nmin} * \code{min.thres} are kept.
+#' 
+#' \item Second step ("intepretation step"): the variables selected by the
+#' first step are considered. \code{nfor.interp} embedded random forests models
+#' are grown, starting with the random forest build with only the most
+#' important variable and ending with all variables selected in the first step.
+#' Then, \code{err.min} the minimum mean out-of-bag (OOB) error of these models
+#' and its associated standard deviation \code{sd.min} are computed.  Finally,
+#' the smallest model (and hence its corresponding variables) having a mean OOB
+#' error less than \code{err.min} + \code{nsd} * \code{sd.min} is selected.
+#' 
+#' \item Third step ("prediction step"): the starting point is the same than in
+#' the second step. However, now the variables are added to the model in a
+#' stepwise manner. \code{mean.jump}, the mean jump value is calculated using
+#' variables that have been left out by the second step, and is set as the mean
+#' absolute difference between mean OOB errors of one model and its first
+#' following model.  Hence a variable is included in the model if the mean OOB
+#' error decrease is larger than \code{nmj} * \code{mean.jump}.  }
+##' @title Variable Selection Using Random Forests
+##' @param formula a formula, such as \code{y~x1 + x2}, where \code{y} is the response variable and anything following \code{~} are predictors. 
+##' @param data the dataset containing the predictors and response. 
+##' @param nruns How many times should random forests be run to compute variable importance? Defaults to 50.
+##' @param silent Should the algorithm talk to you?
+##' @param importance Either "permutation" or "gini." 
+##' @param nmin Number of times the "minimum value" is multiplied to set
+##' threshold value. 
+##' @param ... other arguments passed to \code{\link{cforest}} or \code{\link{randomForest}}
+#'@return The object returned has the following
+#'attributes:
+#'@return \item{variable.importance}{A sorted vector of each variable importance measures.}
+#'@return \item{importance.sd}{the standard deviation of variable importance, measured across the \code{nruns} iterations. }
+#'@return \item{stepwise.error}{The OOB error after each variable is added to the model}
+#'@return \item{response}{The response variable that was modeled.}
+#'@return \item{variables}{A vector of strings that indicate which variables were included in the initial model.}
+#'@return \item{nruns}{How many times the random forest was initially run.}
+#'@return \item{formula}{the formula used for the last model.}
+#'@return \item{data}{the dataset used to fit the model.}
+#'@return \item{oob}{the oob error of the entire model.}
+#'@return \item{time}{how long the algorithm ran for}
+#'@return \item{rfmodel}{The final model used, a randomForest object.}
+#'@import randomForest 
+#' @author Robin Genuer, Jean-Michel Poggi and Christine Tuleau-Malot, with modifications by Dustin Fife
+#' @references 
+#' Genuer, R. and Poggi, J.M. and Tuleau-Malot, C. (2010), Variable
+#' selection using random forests, Pattern Recognition Letters 31(14),
+#' 2225-2236
+#' Carolin Strobl, Anne-Laure Boulesteix, Achim Zeileis, and Torsten Hothorn. Bias in random forest variable importance measures: Illustrations, sources and a solution. 
+#' BMC Bioinformatics, 8(1): 1-21, 2007. doi: 10.1186/1471-2105-8-25. URL http://dx.doi.org/10.1186/1471-2105-8-25.
+#' @rdname rfThresh
+#' @export rfThresh
+#' @seealso \code{\link{rfInterp}}, \code{\link{rfPred}}
+# data(iris); data = iris; formula = as.formula("Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width"); nruns=5; silent=FALSE; importance="gini";nmin=1
+rfThresh = function(formula, data, nruns = 50, silent=FALSE, importance="permutation", nmin=1,...){
+	
+	#### extract varaibles and response from formula
+	vars = attr(terms(formula), "term.labels")
+	resp = row.names(attr(terms(formula), "factors"))[1]
+	
+	#### keep us posted
+	if (!silent){
+		cat(paste("Running Random Forests", nruns, "Times\n"))
+	}
+	
+	start = Sys.time()
+
+	##### get important packages	
+	require(party)
+	require(rpart)
+	require(randomForest)
+	
+	##### check importance
+	importance = match.arg(importance, c("permutation", "gini", NULL))
+
+	##### loop through and compute variable importance nruns times
+	preds = data.frame(matrix(nrow=nruns, ncol=length(vars)))
+	names(preds) = vars
+	oob = 1:length(nruns)	
+
+	##### compute importance 50 times
+	if (importance=="gini"){
+		for (i in 1:nrow(preds)){
+			rf = randomForest(formula, data=data, importance=TRUE,...)
+			preds[i,] = rf$importance[, ncol(rf$importance)-1]
+			if (rf$type=="regression"){
+				oob[i] =  tail(rf$mse, n=1)
+			} else {
+				oob[i] =  tail(rf$err.rate[,1], n=1)
+			}
+		}
+	} else if (importance=="permutation"){
+		for (i in 1:nrow(preds)){
+			mt = sqrt(length(vars))
+			rf = cforest(formula, data=data, controls=cforest_unbiased(ntree=1000, mtry=mt),...)
+
+				##### compute variable importance
+			imp = varimp(rf)
+			preds[i,] = imp
+			oobError = predict(rf, OOB=T)
+			oob[i] = 1-length(which(oobError==data[,resp]))/length(data[, resp])
+		}
+	} else {
+		for (i in 1:nrow(preds)){
+			mt = sqrt(length(vars))
+			rf = cforest(formula, data=data, controls=cforest_unbiased(ntree=1000, mtry=mt),...)
+				##### compute variable importance
+			preds[i,] = NA
+			oobError = predict(rf, OOB=T)
+			oob[i] = 1-length(which(predict(rf, OOB=T)==data[,resp]))/length(data[, resp])
+		}
+	}
+	
+	vimp = colMeans(preds)
+	vimp.sd = apply(preds, 2, sd)
+	
+	#### reorder
+	ord = order(vimp, decreasing=T)
+	ord.sd = vimp.sd[ord]
+	ord.imp = vimp[ord]
+	
+	#### estimate tree (for threshold)
+	s <- NULL
+	if (length(vars)==1) {
+		s <- 1
+	} else {
+	
+		p <- length(vars)
+		u <- 1:p
+		u <- as.data.frame(u)
+
+		# estimation of the standard deviations curve with CART (using "rpart" package)
+
+		# construction of the maximal tree and search of optimal complexity
+		tree <- rpart(ord.sd ~., data=u, control=rpart.control(cp=0, minsplit=2))
+		d <- tree$cptable
+		argmin.cp <- which.min(d[,4])
+		
+		# pruning
+		pruned.tree <- prune(tree, cp=d[argmin.cp, 1])
+		pred.pruned.tree <- predict(pruned.tree)
+		
+		# determination of the y-value of the lowest stair: this is the estimation
+		# of the mean standard deviation of IV
+		min.pred <- min(pred.pruned.tree)
+		
+		# thresholding: all variables with IV mean lower than min.pred are discarded
+		w <- which(ord.imp < nmin*min.pred)
+		
+		if (length(w)==0) {
+		  s <- p
+		}
+		else {
+		  s <- min(w)-1
+		}
+	}
+	
+	formula = make.formula(resp, names(ord.imp[1:s]))
+	if (importance=="gini"){
+		model = randomForest(formula, data=data, importance=TRUE,...)
+	} else {
+		model = cforest(formula, data=data, controls=cforest_unbiased(ntree=1000, mtry=mt),...)
+	}
+	
+	time = Sys.time()-start
+
+	##### make a list to store important info
+	ret = list(importance.mean=ord.imp[1:s], importance.sd=ord.sd[1:s], 
+			response=resp, remaining.variables=names(ord.imp)[1:s], nruns=nruns, formula = formula,
+			data=data, oob=oob, time=time, all.vars=names(ord.imp), model=model)
+	
+	##### make into proper class
+	attr(ret, "class") = c("rfThresh")
+	
+	return(ret)
+}
+
+
+#' @title Print rfThesh Summary
+#' @aliases print.rfThresh
+#' @param x an rfThresh object
+#' @param ... ignored
+#' @method print rfThresh
+#' @S3method print rfThresh
+print.rfThresh = function(x,...){
+	print(names(x))
+	cat(paste("\n\nThe best variables (in order of importance) were:\n\n", sep=""))
+	print(sort(x$importance.mean, decreasing=TRUE))
+		cat(paste("\n\n"))
+	print(x$time)
+}	
+
+#' @title Plot rf.thesh Summary
+#' @aliases plot.rfThresh
+#' @param x an rfThresh object
+#' @param y igorned
+#' @param ... other parameters passed to plo
+#' @method plot rfThresh
+#' @S3method plot rfThresh
+plot.rfThresh = function(x, y, ...){
+	length.vars = length(x$importance.mean)
+	var = x$importance.sd
+	vimp = x$importance.mean
+	labels = list(xlab="", ylab="Variable Importance", pch=16, cex=.8, xlim=range(.5, length.vars+.5), xaxt="n") 
+	args = modifyList(labels, list(x=1:length.vars, y=vimp,...))
+	do.call("plot", args)	
+	text(1:length.vars, vimp, names(vimp), pos=4, cex=.8)
+	segments(1:length.vars, vimp+1.96*var, 1:length.vars, vimp-1.96*var, col="lightgray")
+}
