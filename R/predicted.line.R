5121d7dedc8afb6af43cf9dfd40fbc50f7fa295f
dustinfife <fife.dustin@gmail.com>
2017-11-21 11:17:55 -0500

    fixed minor arrors in predicted.line
    fixed minor errors in predicted.line

--- R/predicted.line.R
+++ R/predicted.line.R
@@ -9,17 +9,26 @@
 ##' @param covariates A function used on all the covariates. Can be mean, median, min, or max
 ##' @param length The number of predicted points used for plotting the line. Defaults to 100. 
 ##' @param Add. Should the fitted line be added to an existing plot?
+##' @param ret.pred Should the fits be returned as a matrix?
 ##' @param ... Other arguments passed to plot, lines, or predict
 ##' @author Dustin Fife
 ##' @export
 ##' @examples
 ##' ## do this later
-predicted.line = function(model, outcome, IV, covs = NA, data, cov.func=c("mean", "median", "min", "max"), length=100, add=F,...){
+predicted.line = function(model, outcome, IV, covs = NA, data, cov.func=c("mean", "median", "min", "max"), length=100, add=F, ret.pred=F, ...){
 
 	d = data
 	##### extract terms
-	dv = all.vars(model$terms)[1]
-	ivs = all.vars(model$terms)[2:length(all.vars(model$terms))]	
+	dv = all.vars(model$call$formula)[1]
+	ivs = all.vars(model$call$formula)[2:length(all.vars(model$call$formula))]	
+
+	##### modify for zero inflated models
+	if (length(names(model$terms))>0){
+	if (names(model$terms)[2]=="zero"){
+		dv = all.vars(model$terms$full)[1]
+		ivs = all.vars(model$terms$full)[2:length(all.vars(model$call$formula))]	
+	}}
+
 
 	##### create new dataset (with same names as d)
 	new.dat = d[1:100,c(dv, ivs)]
@@ -37,11 +46,14 @@ predicted.line = function(model, outcome, IV, covs = NA, data, cov.func=c("mean"
 	new.dat[,IV] = seq(from=min(iv.vals, na.rm=T), to=max(iv.vals, na.rm=T), length.out=100)
 	
 	#### predict DV
-	new.dat[,outcome] = predict(model, new.dat, ...)
-	
+	new.dat[,outcome] = predict(model, new.dat, type="response", ...)
 	##### plot the line
 	if (!add){
-		plot(iv.vals, dv.vals, ...)
+		plot(d[,IV], d[,outcome], ...)
 	}
 	lines(new.dat[,IV], new.dat[,outcome], ...)
+	
+	if (ret.pred){
+		return(new.dat)
+	}
 }
