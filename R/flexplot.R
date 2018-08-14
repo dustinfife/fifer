bb6be88889fcc1aeac014644cedbb3328d192103
dustinfife <fife.dustin@gmail.com>
2018-08-14 15:27:25 -0400

    debug flexplot
    debug flexplot

--- R/flexplot.R
+++ R/flexplot.R
@@ -43,7 +43,7 @@ flexplot = function(formula, data,
 		numbers = names(which(unlist(lapply(data[,predictors], is.numeric))))
 		categories = names(which(!(unlist(lapply(data[,predictors], is.numeric)))))
 	}
-	
+
 		### remove missing values
 	if (length(predictors)>0){
 		if (length(unlist(apply(data[,variables], 2, function(x){(which(is.na(x)))})))>0){
@@ -61,11 +61,13 @@ flexplot = function(formula, data,
 	}
 
 	### if they don't want raw data, just make alpha = 0
-	if (raw.data){
-		alpha.raw = .35
-	} else {
-		alpha.raw = 0
-	}	
+	raw.alph.func = function(raw.data,alpha=1){
+		if (raw.data){
+			alpha.raw = alpha
+		} else {
+			alpha.raw = 0
+		}	
+	}
 
 	### BEGIN THE MEGA PLOTTING IFS!
 	### PLOT UNIVARIATE PLOTS
@@ -73,15 +75,32 @@ flexplot = function(formula, data,
 	if (length(outcome)==1 & length(predictors)==0 & length(given)==0){
 		p = uni.plot(outcome, d=data)
 
-	#### BIVARIATE PLOTS	
-	} else if (length(outcome)==1 & length(predictors)==1 & length(given)==0){			
-		p = bivariate.plot(predictors, outcome, d=data, jitter=jitter, raw.data=raw.data, spread=spread, method=method, se=se, sample=sample)
+	#### SCATTERPLOT	
+	} else if (length(outcome)==1 & length(predictors)==1 & length(given)==0 & (is.numeric(data[,predictors]) & is.numeric(data[,outcome]))){			
+		p = ggplot(data=data, aes_string(x=predictors, y=outcome))+
+			geom_point(data=sample.subset(sample, data), alpha=raw.alph.func(raw.data)) +
+			geom_smooth(method=method, se=se) +
+			theme_bw()						
 
+	##### MEAN PLOT
+	} else if (length(outcome)==1 & length(predictors)==1 & length(given)==0 & (is.numeric(data[,predictors]) | is.numeric(data[,outcome]))){		
+				
+		p = ggplot(data=data, aes_string(x=predictors, y=outcome)) +
+			geom_jitter(data=sample.subset(sample, data), alpha=raw.alph.func(raw.data, .15), size=.75, width=.05) + 
+			stat_summary(fun.y='median', geom='point', size=2, color='red') + 
+			stat_summary(aes_string(x=predictors, y=outcome), geom='errorbar', fun.ymin = function(z){quantile(z, .25)}, fun.ymax = function(z) {quantile(z, .75)}, fun.y=median, color='red', width=.2)+
+			theme_bw()
+	
+	##### CHI SQUARE PLOT
+	} else if (length(outcome) == 1 & length(predictors)==1 & length(given)==0 & !is.numeric(data[,predictors]) & !is.numeric(data[,outcome])){
+		m = as.data.frame(table(d[,predictors], d[,outcome])); names(m)[1:2] = c(predictors, outcome)
+		Freq = 'Freq'
+		p = ggplot(data=m, aes_string(x=predictors, y=Freq, fill=outcome)) + geom_bar(stat='identity', position='dodge') + theme_bw()
 
 	##### INTERACTION PLOT
 	} else if (length(outcome)==1 & length(predictors)==2 & (is.character(data[,predictors[1]]) | is.factor(data[,predictors[1]])) & (is.character(data[,predictors[2]]) | is.factor(data[,predictors[2]]))){		
 		p = ggplot(data=data, aes_string(x=predictors[1], y=outcome)) +
-			geom_jitter(data=sample.subset(sample, data), alpha = alpha.raw, size=.25, width=.2) +
+			geom_jitter(data=sample.subset(sample, data), alpha = raw.alph.func(raw.data, .35), size=.25, width=.2) +
 			facet_wrap(as.formula(paste("~", predictors[2]))) +
 			stat_summary(aes_string(x=predictors[1], y=outcome), geom="errorbar", fun.ymin = function(z){mean(z) - 1.96*(sd(z)/length(z))}, fun.ymax=function(z){mean(z) + 1.96*(sd(z)/length(z))}, fun.y = mean, width=.1) +
 			stat_summary(fun.y = "mean", geom="line", group=1) +
@@ -90,15 +109,52 @@ flexplot = function(formula, data,
 		
 	#### ANCOVA PLOT		
 	} else if (length(predictors)==2 & length(categories)==1){
-		p = ggplot(data=d, aes_string(x=numbers, y=outcome, group=categories, linetype=categories)) +
-			geom_point(data=sample.subset(sample, data), alpha=alpha.raw) +
+		p = ggplot(data=d, aes_string(x=numbers, y=outcome, group=categories, linetype=categories, color=categories)) +
+			geom_point(data=sample.subset(sample, data), alpha=raw.alph.func(raw.data)) +
 			geom_smooth(method=method, se=se) +
 			theme_bw()							
 
+	###### MULTIWAY DOT PLOT FOR THREE CATEGORICAL PREDICTORS
+	} else if (length(outcome)==1 & length(predictors)==3 & length(categories)==3){
 
+	
+		#### figure out which variable has the largest effect
+		mod = lm(formula, data=data)
+
+		order = order(anova(mod)[-length(coef(mod)),"F value"], decreasing=F)
+		ordered.predictors = row.names(anova(mod))[order]
+		new.formula = make.formula(outcome, ordered.predictors)
+		
+		#### reorder the means
+		means = aggregate(new.formula, data=data, FUN=mean)
+		names(means)[ncol(means)]="mean"
+		ser = aggregate(new.formula, data=data, FUN=function(x){sd(x)})
+		means$ser = ser[,outcome]
+		means$lower = means$mean - means$ser
+		means$upper = means$mean + means$ser
+
+		#### combine with previous dataset
+		d = merge(data, means, by=ordered.predictors)
+		if (se){
+			p = ggplot(data=d, aes_string(x= ordered.predictors[1], y=outcome, col=ordered.predictors[1])) + 
+				geom_jitter(data=sample.subset(sample, d), alpha = raw.alph.func(raw.data, .15)) +
+				geom_point(aes_string(y="mean")) + 
+				geom_errorbar(aes_string(ymin="lower", ymax= "upper", col=ordered.predictors), width=.2) +
+				coord_flip() + 
+				facet_grid(as.formula(paste0(ordered.predictors[2]," +", ordered.predictors[3],"~."))) +
+				theme_bw()
+		} else {
+			p = ggplot(data=d, aes_string(x= ordered.predictors[1], y=outcome, col=ordered.predictors[1])) + 
+				geom_jitter(data=sample.subset(sample, d), alpha = raw.alph.func(raw.data, .15)) +
+				geom_point(aes_string(y="mean")) + 
+				coord_flip() + 
+				facet_grid(as.formula(paste0(ordered.predictors[2]," +", ordered.predictors[3],"~."))) +
+				theme_bw()			
+		}				
+		
 	##### FOR VARAIBLES THAT WILL BE BINNED...
 	} else {
-		
+		print(head(data))
 		##### only allow two "given" variables
 		if (length(given)>2){
 			stop("Only two 'given' variables are allowed.")
@@ -160,7 +216,6 @@ flexplot = function(formula, data,
 
 		
 		if (length(axis)>1){
-			print("here")
 			p = ggplot(data=data, aes_string(x=axis[1], y=outcome, symbol=axis[1], linetype=axis[2]))+
 					geom_point(alpha=0) +
 					geom_smooth(method=method, se=se, col="black") + 
@@ -179,71 +234,5 @@ flexplot = function(formula, data,
 	
 
 	}		
-			return(p)	
-		
-		
-		
-		
-		
-		
-		
-		# if (length(outcome)==1 & length(predictors)==3 & (is.character(data[,predictors[1]]) | is.factor(data[,predictors[1]])) & (is.character(data[,predictors[2]]) | is.factor(data[,predictors[2]])) & (is.character(data[,predictors[3]]) | is.factor(data[,predictors[3]]))){
-	# ###### MULTIWAY DOT PLOT FOR THREE CATEGORICAL PREDICTORS
-	
-		# #### figure out which variable has the largest effect
-		# mod = lm(formula, data=data)
-		# anova(mod)
-		# order = order(anova(mod)[-length(coef(mod)),"F value"], decreasing=F)
-		# ordered.predictors = row.names(anova(mod))[order]
-		# new.formula = make.formula(outcome, ordered.predictors)
-		# #### reorder the means
-		# means = aggregate(new.formula, data=data, FUN=mean)
-		# names(means)[ncol(means)]="mean"
-		# se = aggregate(new.formula, data=data, FUN=function(x){sd(x)/length(x)})
-		# means$se = se[,outcome]
-
-		# #### combine with previous dataset
-		# d = merge(data, means, by=ordered.predictors)
-
-
-		# call = paste0("ggplot(data=", deparse(substitute(d)), ", aes(x=", ordered.predictors[1], ", y=", outcome, ", col=", ordered.predictors[1], ")) + geom_point() +
-		# geom_errorbar(aes(ymin=mean - 1.96*se, ymax= mean + 1.96*se, col=",ordered.predictors[1], "), width=.2) +
-		# coord_flip(ylim=c(0,1)) + 
-		# facet_wrap(",ordered.predictors[2],"+", ordered.predictors[3],"~., ncol=1)
-		# " )
-		# cat(call)
-		# eval(parse(text = call))	
-		# +
-						# geom_errorbar(aes_string(ymin= outcome - 1.96* outcome*(1-outcome)/sqrt(nrow(d)), ymax= outcome + 1.96* outcome*(1-outcome)/sqrt(nrow(d)),col=ordered.predictors[1]), width=.2) + 
-						# coord_flip(ylim=c(0,1)) + 
-						# facet_wrap(~Condition, ncol=1) + 
-						# scale_color_manual(values=c("green", "red", "blue"))+
-						# theme_bw() +
-						# theme(strip.text = element_text(size=15))							
-				
-				
-				
-				# a = ggplot(data=d[d$Category=="Health",], aes(x=Group, y=Percentage, col=Group)) + geom_point()+ 
-						# geom_errorbar(aes(ymin=Percentage - 1.96*Percentage*(1-Percentage)/sqrt(200), ymax=Percentage + 1.96*Percentage*(1-Percentage)/sqrt(200),col=Group), width=.2) + 
-						# coord_flip(ylim=c(0,1)) + 
-						# facet_wrap(~Condition, ncol=1) + 
-						# scale_color_manual(values=c("green", "red", "blue"))+
-						# theme_bw() +
-						# theme(strip.text = element_text(size=15))
-				# a		
-		
-		
-		
-		
-		
-	
-		
-		
-	#}
-	
-	## GIVEN VARIABLES
-	
-	
+	return(p)
 }
-
-
