ae26321dad9cd2393b1aa58d929166c94fce3083
dustinfife <fife.dustin@gmail.com>
2019-02-14 11:27:41 -0500

    added make.data function

---
+++ R/make.data.R
@@ -0,0 +1,41 @@
+##' Basic function for simulating bivariate data
+##'
+##' Basic function for simulating bivariate data
+##'	
+##' This can model either numeric on numeric data or categorical on numeric. 
+##' @param cor The correlation coefficient between the Y and X
+##' @param means A vector of length two that indicates the mean of Y and X, respectively
+##' @param sds A vector of length two that indicates the standard deviations of Y and X, respectively
+##' @param n Either a single value or a vector indicating the sample size of each group
+##' @param names The names of the Y and X variables, respectively
+##' @param groups The names of the groups
+##' @return a simulated dataset
+##' @author Dustin Fife
+##' @export
+##' @examples
+##' fake.data = make.data(cor=.6, means=c(50, 100), sds = c(15, 15), n=100, names=c("Exam Score", "IQ"))
+##' flexplot(IQ~Exam.score, data=fake.data)
+##' 
+##' fake.data = make.data(means = c(10, 50, 30), sds=c(4, 15, 9), n=c(20, 26, 55), names=c("Depression", "Condition"), groups=c("Control", "Medication + Therapy", "Therapy"))
+##' flexplot(Depression~Condition, data=fake.data)
+make.data = function(cor=NULL, means, sds, n, names=c("X","Y"), groups=c("A", "B")){
+	if (!is.null(cor)){
+		cor.mat = matrix(c(1, cor, cor, 1), nrow=2)
+		cov.mat = cor2cov(cor.mat, sds)
+		d = data.frame(mvrnorm(n, means, cov.mat))
+		names(d) = names
+		return(d)
+	} else {
+		### generate categorical variable
+		outcome = 1:(sum(n)); group.vals=rep("NN", times=length(outcome))
+		end.row = 1
+		i = 2
+		for (i in 1:length(n)){
+			outcome[end.row:(end.row+n[i]-1)] = rnorm(n[i], means[i], sds[i])
+			group.vals[end.row:(end.row+n[i]-1)] = groups[i]
+			end.row = end.row + n[i]
+		}
+		d = data.frame(x=outcome, groups=group.vals); names(d) = names
+		return(d)
+	}
+}
