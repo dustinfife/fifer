d80590e7567112a30786a653fb2afc7c5e11512a
dustinfife <fife.dustin@gmail.com>
2019-02-22 10:16:55 -0500

    modified make.data

--- R/make.data.R
+++ R/make.data.R
@@ -10,6 +10,7 @@
 ##' @param names The names of the Y and X variables, respectively
 ##' @param groups The names of the groups
 ##' @param digits The number of digits the numeric variables should be rounded to
+##' @cap The min/max values for each of the variables, to be used for truncating scores, coded as a vector. 
 ##' @return a simulated dataset
 ##' @author Dustin Fife
 ##' @export
@@ -19,13 +20,47 @@
 ##' 
 ##' fake.data = make.data(means = c(10, 50, 30), sds=c(4, 15, 9), n=c(20, 26, 55), names=c("Depression", "Condition"), groups=c("Control", "Medication + Therapy", "Therapy"))
 ##' flexplot(Depression~Condition, data=fake.data)
-make.data = function(cor=NULL, means, sds, n, names=c("X","Y"), groups=c("A", "B"), digits=0){
+make.data = function(cor=NULL, means, sds, n, names=c("X","Y"), groups=c("A", "B"), digits=0, cap=NULL){
 	if (!is.null(cor)){
 		cor.mat = matrix(c(1, cor, cor, 1), nrow=2)
 		cov.mat = cor2cov(cor.mat, sds)
 		d = data.frame(mvrnorm(n, means, cov.mat))
 		names(d) = names
 		d = round(d, digits=digits)
+	
+		if (!is.null(cap)){
+			### max for y
+			if (!is.null(cap[1])){
+				mx.y = which(d[,1]<cap[1])
+				if (length(mx.y)>0){
+					d[mx.y,1] = cap[1]
+				}
+			}
+	
+			### min for y
+			if (!is.null(cap[2])){
+				mx.y = which(d[,1]>cap[2])
+				if (length(mx.y)>0){
+					d[mx.y,1] = cap[2]
+				}
+			}			
+			
+			### max for y
+			if (!is.null(cap[3])){
+				mx.y = which(d[,2]<cap[3])
+				if (length(mx.y)>0){
+					d[mx.y,2] = cap[3]
+				}
+			}
+	
+			### min for y
+			if (!is.null(cap[4])){
+				mx.y = which(d[,2]>cap[4])
+				if (length(mx.y)>0){
+					d[mx.y,2] = cap[4]
+				}
+			}
+		}
 		return(d)
 	} else {
 		### generate categorical variable
@@ -39,6 +74,27 @@ make.data = function(cor=NULL, means, sds, n, names=c("X","Y"), groups=c("A", "B
 		}
 		d = data.frame(x=outcome, groups=group.vals); names(d) = names
 		d[,1] = round(d[,1], digits=digits)
+
+		if (!is.null(cap)){
+			### max for y
+			if (!is.null(cap[1])){
+				mx.y = which(d[,1]<cap[1])
+				if (length(mx.y)>0){
+					d[mx.y,1] = cap[1]
+				}
+			}
+	
+			### min for y
+			if (!is.null(cap[1])){
+				mx.y = which(d[,1]>cap[2])
+				if (length(mx.y)>0){
+					d[mx.y,1] = cap[2]
+				}
+			}			
+		
 		return(d)
 	}
+	}
 }
+
+cap = c(0,100,0,NULL)
