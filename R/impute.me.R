371eea72f47d82efa0f512d62f10880f73326193
dustinfife <fife.dustin@gmail.com>
2019-05-28 12:03:16 -0400

    added impute.model.comparison

--- R/impute.me.R
+++ R/impute.me.R
@@ -78,21 +78,7 @@ impute.me = function(model, data, predictors=NULL, keep=T, imputations=20, silen
 ##' @import mice
 ##' @author Dustin Fife
 ##' @export
-##' @examples
-##' data(exercise_data)
-##' d = exercise_data
-##' 
-##' 		##### create missing data in motivation
-##' missing.ld = which(d$motivation<quantile(d$motivation, .25))
-##' notmissing = which(!(1:nrow(d) %in% missing.ld))
-##' d$weight.change.missing = d$weight.change
-##' d$weight.change.missing[missing.ld] = NA
-##' 
-##' 		#### create model with missing data
-##' mod = lm(weight.change.missing~motivation, data=d, model=T)
-##' predictors = c("muscle.gain.missing", "weight.change")
-##' impute.me(mod, data=d, predictors=predictors, keep=F, imputations=5)
-impute.bf = function(model1, model2, data,predictors=NULL, keep=T, imputations=20, silent=F, invert=F){
+impute.model.comparison = function(model1, model2, data,predictors=NULL, keep=T, imputations=20, silent=F, invert=F){
 	impute.me.full = impute.me(model1, data=data, predictors=predictors, keep=keep, imputations=imputations, silent=F, return.mod=T)
 	impute.me.reduced = impute.me(model2, data=data, predictors=predictors, keep=keep, imputations=imputations, silent=F, return.mod=T)	
 	
@@ -100,9 +86,24 @@ impute.bf = function(model1, model2, data,predictors=NULL, keep=T, imputations=2
 	bic.full = log(nrow(data))*length(compared$qbar1)-compared$deviances$dev1.M[1]
 	bic.reduced = log(nrow(data))*length(compared$qbar0)-compared$deviances$dev0.M[1]
 	bf = exp((bic.full-bic.reduced)/2)
+	
+	### do same for r squared
+	rsq.full = pool.r.squared(impute.me.full$models)
+	rsq.reduced = pool.r.squared(impute.me.reduced$models)	
+
 	if (invert){
 		1/bf
 	} else {
 		bf
 	}
+
+
+	### create a table
+	results.table = data.frame(model=c("Full", "Reduced"), rsq = NA, BIC=NA, BF = NA, p=NA)
+	results.table$rsq = c(rsq.full[1], rsq.reduced[1])
+	results.table$BIC = c(bic.full, bic.reduced)
+	results.table$BF[1] = bf
+	results.table$p[1] = compared$pvalue
+
+	return(results)	
 }
